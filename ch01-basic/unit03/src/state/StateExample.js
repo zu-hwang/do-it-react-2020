3cae7a046d14cb346110d051113956ae5a428946
zu-hwang <tropical.kitsch@gmail.com>
2020-06-16 20:10:50 +0900

    폴더정리

---
+++ ch01-basic/unit03/src/state/StateExample.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class StateExample extends React.Component {
+  constructor(props) {
+    super(props);
+    // state 정의
+    this.state = {
+      loading: true,
+      formData: 'no Data',
+    };
+    // 화살표함수없이 this를 사용한다면 .bind()필수사용해야 this가 가르키는 곳을 확실하게 할 수 있다.
+    this.handleData = this.handleData.bind(this);
+    // 4초뒤에 this.handleData 함수 실행
+    setTimeout(this.handleData, 4000);
+  }
+  handleData() {
+    const data = 'new data';
+    const { formData } = this.state;
+    // state 변경
+    this.setState({
+      loading: false,
+      formData: data + formData,
+    });
+    console.log('loading값', this.state.loading);
+  }
+
+  render() {
+    return (
+      <div>
+        {/* state 데이터는 this.state로 접근 가능 */}
+        <span>로딩중 : {String(this.state.loading)}</span>
+        <spna>결과 : {this.state.formData}</spna>
+      </div>
+    );
+  }
+}
+
+export default StateExample;
