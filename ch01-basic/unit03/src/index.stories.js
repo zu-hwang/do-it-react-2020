2013b017314e72945bcf5771d3b475d94b9f81f5
zu-hwang <tropical.kitsch@gmail.com>
2020-06-17 13:01:42 +0900

    wip

---
+++ ch01-basic/unit03/src/index.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@storybook/react/demo';
+
+export default { title: 'Button' };
+export const withText = () => <Button>Hello Button</Button>;
+export const withEmoji = () => (
+  <Button>
+    <span role='img' aria-label='so cool'>
+      🥰🙃🧐🥺
+    </span>
+  </Button>
+);
